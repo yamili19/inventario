03836e4ea507b3520ab9d2854bf3ef96bb0828c7
yamiliyarbouh <yamiliyarbouh@gmail.com>
2023-01-07 19:44:38 -0300

    creer el archivo de configuracion para la cadena de conexion a la base de datos

---
+++ pav-3k2-2022-11-main/TP1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using TP.Interfaces;
+
+namespace TP
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
